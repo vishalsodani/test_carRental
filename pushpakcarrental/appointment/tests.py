2a54d03d7f2a653e5d507ed8edd041f99f88e78e
sparamk <sparamk@ilstu.edu>
2022-12-05 21:36:01 -0600

    more files

--- pushpakcarrental/appointment/tests.py
+++ pushpakcarrental/appointment/tests.py
@@ -1,80 +1,81 @@
-# from django.contrib.auth.models import User
-# from django.test import TestCase, Client
-# from appointment.models import *
+        
+from django.contrib.auth.models import User
+from django.test import TestCase, Client
+from appointment.models import *
 
 
-# class appointmentTestCases(TestCase):
+class appointmentTestCases(TestCase):
 
-#     def test_schedule_appointment(self):
-#         print(self._testMethodName)
-#         newUser = User.objects.create_user(username="driver", email="driver@gmail.com", password="drive")
-#         data={
-#             "id":newUser.id,
-#             'appointment_time':'05:07',
-#             "appointment_date":'2022-07-06'
-#             }
+    def test_schedule_appointment(self):
+        print(self._testMethodName)
+        newUser = User.objects.create_user(username="driver", email="driver@gmail.com", password="drive")
+        data={
+            "id":newUser.id,
+            'appointment_time':'05:07',
+            "appointment_date":'2022-07-06'
+            }
             
-#         User.objects.filter(id=newUser.id)
-#         appointment = Appointment_data()
-#         appointment.user_id = newUser.id
-#         appointment.appt_time = "05:07"
-#         appointment.appt_date = "2022-07-06"
-#         appointment.purpose = "appointment"
-#         appointment.save()
+        User.objects.filter(id=newUser.id)
+        appointment = Appointment_data()
+        appointment.user_id = newUser.id
+        appointment.appt_time = "05:07"
+        appointment.appt_date = "2022-07-06"
+        appointment.purpose = "appointment"
+        appointment.save()
 
-#         self.client.login(username='driver', password='drive')
-#         data = {
-#             'username': "robot",
-#             'firstname': "machine",
-#             'lastname': "learning",
-#             'email': 'robot@gmail.com',
-#             'id': newUser.id,
-#             'appt_time': "12pm",
-#             'appt_date': "12-05-2022"
-#         }
+        self.client.login(username='driver', password='drive')
+        data = {
+            'username': "robot",
+            'firstname': "machine",
+            'lastname': "learning",
+            'email': 'robot@gmail.com',
+            'id': newUser.id,
+            'appt_time': "12pm",
+            'appt_date': "12-05-2022"
+        }
 
-#         response = self.client.post('/scheduleappointment/', data=data)
-#         # Check that the response is 200 OK.
-#         self.assertEqual(Appointment_data.objects.get(user_id=), "appointment")
+        response = self.client.post('/scheduleappointment/', data=data)
+        # Check that the response is 200 OK.
+        self.assertEqual(Appointment_data.objects.get(user_id=), "appointment")
 
-#     def test_schedule_testdrive(self):
-#         print(self._testMethodName)
-#         newUser = User.objects.create_user(username="robot", email="robot@gmail.com", password="pass")
-#         newUser.first_name = "machine"
-#         newUser.last_name = "language"
-#         newUser.save()
+    def test_schedule_testdrive(self):
+        print(self._testMethodName)
+        newUser = User.objects.create_user(username="robot", email="robot@gmail.com", password="pass")
+        newUser.first_name = "machine"
+        newUser.last_name = "language"
+        newUser.save()
 
-#         self.client.login(username='robot', password='pass')
-#         data = {
-#             'username': "robot",
-#             'firstname': "machine",
-#             'lastname': "learning",
-#             'email': 'robot@gmail.com',
-#             'id': newUser.id,
-#             'appt_time': "12pm",
-#             'appt_date': "12/05/2022"
-#         }
+        self.client.login(username='robot', password='pass')
+        data = {
+            'username': "robot",
+            'firstname': "machine",
+            'lastname': "learning",
+            'email': 'robot@gmail.com',
+            'id': newUser.id,
+            'appt_time': "12pm",
+            'appt_date': "12/05/2022"
+        }
 
-#         response = self.client.post('/scheduleappointment/', data=data)
-#         self.assertEqual(Appointment_data.objects.filter(user_id=newUser.id).purpose, "appointment")
+        response = self.client.post('/scheduleappointment/', data=data)
+        self.assertEqual(Appointment_data.objects.filter(user_id=newUser.id).purpose, "appointment")
 
-#     def test_schedule_testdrive_date(self):
-#         print(self._testMethodName)
-#         newUser = User.objects.create_user(username="robot", email="robot@gmail.com", password="pass")
-#         newUser.first_name = "machine"
-#         newUser.last_name = "language"
-#         newUser.save()
+    def test_schedule_testdrive_date(self):
+        print(self._testMethodName)
+        newUser = User.objects.create_user(username="robot", email="robot@gmail.com", password="pass")
+        newUser.first_name = "machine"
+        newUser.last_name = "language"
+        newUser.save()
 
-#         self.client.login(username='robot', password='pass')
-#         data = {
-#             'username': "robot",
-#             'firstname': "machine",
-#             'lastname': "learning",
-#             'email': 'robot@gmail.com',
-#             'id': newUser.id,
-#             'appt_time': "12pm",
-#             'appt_date': "12/05/2022"
-#         }
+        self.client.login(username='robot', password='pass')
+        data = {
+            'username': "robot",
+            'firstname': "machine",
+            'lastname': "learning",
+            'email': 'robot@gmail.com',
+            'id': newUser.id,
+            'appt_time': "12pm",
+            'appt_date': "12/05/2022"
+        }
 
-#         response = self.client.post('/scheduletestdrive/', data=data)
-#         self.assertEqual(Appointment_data.objects.filter(user_id=newUser.id).appt_date, data["appt_date"])
+        response = self.client.post('/scheduletestdrive/', data=data)
+        self.assertEqual(Appointment_data.objects.filter(user_id=newUser.id).appt_date, data["appt_date"])
--- pushpakcarrental/user/tests.py
+++ pushpakcarrental/user/tests.py
@@ -1,3 +1,92 @@
-from django.test import TestCase
+import unittest
 
-# Create your tests here.
+from django.test import TestCase
+from django.contrib.auth.models import User, Group
+from django.contrib.auth import authenticate
+from django.contrib import auth
+
+from django.test import TestCase, Client
+
+class userTestCases(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        User.objects.create()
+            
+    def test_signUp(self):
+        print(self._testMethodName)
+        newUser=User.objects.create_user("robot", "robot@gmail.com", "pass")
+        data= {
+        "username":"robot",
+        "firstname":"machine",
+        "lastname":"language",
+        "email":"shashiteja441@gmail.com",
+        "password" : "pass",
+        "confirm-password" : "pass",
+        "is_active": True
+        }   
+        response = self.client.post('/signup/', data=data)
+        # Check that the response is 200 OK.
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(User.objects.get(username="robot"))
+
+    def test_Login(self):
+        print(self._testMethodName)
+        data ={
+            "username":"robot",
+            "password":"passs"
+        }
+        
+        user = authenticate(username="robot",password="pass")
+        response = self.client.post('/login/', data=data)
+        # Check that the response is 200 OK.
+        self.assertEqual(response.status_code, 200)
+    
+    def test_modifyProfile(self):
+        print(self._testMethodName)
+        newUser=User.objects.create_user(username="robot",email="robot@gmail.com",password="pass")
+        newUser.first_name="machine"
+        newUser.last_name="language"
+        newUser.save()
+        
+        self.client.login(username='robot', password='pass')
+        data= {
+            'username':"robot",
+            'firstname':"machine",
+            'lastname':"learning",
+            'email':'robot@gmail.com'
+        }
+        
+        response = self.client.post('/modifyprofile/', data=data)
+        # Check that the response is 200 OK.
+        self.assertEqual(response.status_code,200)
+        self.assertEqual(User.objects.get(username="robot").last_name,"learning")    
+        
+    def test_changeProfilePassword(self):
+        print(self._testMethodName)
+        User.objects.create_user(username="robot",email="robot@gmail.com",password="pass")
+        user = User.objects.get(username = "robot")
+        user.set_password("newPass")
+        user.save()
+        data= {
+            "username" : "robot",
+            "password" : "newPass",
+            "success" : "success"
+        }
+        self.assertTrue(self.client.login(username='robot', password='newPass'))
+        response = self.client.post('/changeprofilepassword/',data=data)
+        # Check that the response is 200 OK.
+        self.assertEqual(response.status_code,200)
+        
+    def test_logout(self):
+        print(self._testMethodName)
+        User.objects.create_user(username="black",email="black@gmail.com",password="white")
+        user = User.objects.get(username = "black")
+        user.save()
+        self.client.login(username='black', password='white')
+        data= {
+            "success" : "successfully logged out"
+        }
+        response = self.client.post('/logout/',data=data)
+        reponse1=self.client.logout()
+        self.assertTrue(response)
